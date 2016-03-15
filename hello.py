340487faa0b69baf19f28a81c44d0fc04d668428
wannabe87 <hiteshnankani1987@gmail.com>
2016-03-16 01:36:21 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
 print "Hello world"
+print ("I was just reading the command line for windows, that does not seem to be used here. ")
+print (" caution: only 'hello world' can give error since python 3.0 uses () also ")
